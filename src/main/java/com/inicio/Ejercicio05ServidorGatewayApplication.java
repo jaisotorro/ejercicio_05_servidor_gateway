dc400b9add63eeefeaadf25d496176cffa7167ba
jaimesto <jaimesto@ext.inditex.com>
2023-12-06 09:25:05 +0100

    Initial commit

---
+++ src/main/java/com/inicio/Ejercicio05ServidorGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.inicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio05ServidorGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicio05ServidorGatewayApplication.class, args);
+	}
+
+}
